c83ce02d9077bc868a2e176d607efd771afb46ed
shira1008 <shaharshira1008@gmail.com>
2023-05-28 17:01:41 +0300

    add promotions

--- products.py
+++ products.py
@@ -11,6 +11,17 @@ class Product:
         self.price = price
         self.quantity = quantity
         self.active = True
+        self.promotion = None
+
+        # for the promotion class
+
+    def get_promotion(self):
+        return self.promotion
+
+        # for the promotion class
+
+    def set_promotion(self, promotion):
+        self.promotion = promotion
 
     def get_quantity(self):
         return self.quantity
@@ -30,7 +41,11 @@ class Product:
         self.active = False
 
     def show(self):
-        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+        if self.promotion:
+            promotion_info = f", Promotion: {self.promotion.name}"
+        else:
+            promotion_info = ""
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}{promotion_info}"
 
     def buy(self, quantity):
         if quantity <= 0:
@@ -41,12 +56,48 @@ class Product:
 
         # if the user want to buy more than the store have
         if quantity > self.quantity:
-            raise ValueError(f"You asked for {quantity} but there are only {self.quantity} available")
+            raise ValueError(
+                f"You asked for {quantity} but there are only {self.quantity} available"
+            )
+
+        # check if have promotion, if have handle this with the changed price:
+        if self.promotion:
+            total_price = self.promotion.apply_promotion(self, quantity)
+        else:
+            total_price = self.price * quantity
 
         self.quantity -= quantity
-        total_price = self.price * quantity
 
         if self.quantity == 0:
             self.set_quantity(0)
 
         return total_price
+
+
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        super().__init__(name, price, quantity=0)
+
+    def set_quantity(self, quantity):
+        pass
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}"
+
+    def buy(self, quantity=0):
+        total_price = self.price
+        return total_price
+
+
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, maximum):
+        super().__init__(name, price, quantity)
+        self.maximum = maximum
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}"
+
+    def buy(self, quantity):
+        if quantity > self.maximum:
+            raise ValueError(f"products can only be purchased {self.maximum} times")
+        return super().buy(quantity)
