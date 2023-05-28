80909c8dbdb54f4b4b33276cd4ee9357c6befc0e
shira1008 <shaharshira1008@gmail.com>
2023-05-28 13:00:57 +0300

    test_file

---
+++ test_product.py
@@ -0,0 +1,8 @@
+import products
+
+def test_create_normal_product():
+    product =  products.Product("MacBook Air M2", price=1450, quantity=100)
+    assert product.name == "MacBook Air M2"
+    assert product.price == 1450
+    assert product.quantity == 100
+
